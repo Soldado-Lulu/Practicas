6c29c70d6bd1dcf7e89ad262a2042b63b7619fe5
Erick <vasquez.castro.andres@gmail.com>
2025-01-01 17:34:29 -0400

    POO completo

---
+++ javascript/objetos/05-referencia.js
@@ -0,0 +1,17 @@
+let a = { prop:1};
+
+function suma(n){
+    n.prop++;
+}
+
+
+suma(a);
+console.log(a);
+
+
+//PRIMITIVOS ---> SE COPIAN 
+
+//REFERENCIA ---> SE REFERENCIA
+                    //-OBJETOS
+                    //-ARRAY
+                    //-FUNCIONES
---
+++ javascript/objetos/06-list-propiedades.js
@@ -0,0 +1,19 @@
+const punto = {
+
+    x:10,
+    y:15,
+    dibujar(){
+        console.log("dibujando....");
+    }
+};
+
+delete punto.dibujar;
+if('dibujar' in punto){
+    punto.dibujar();
+}
+
+console.log(Object.keys(punto));
+
+for(let llave of Object.keys(punto)){
+    console.log(llave, punto[llave]);
+}
