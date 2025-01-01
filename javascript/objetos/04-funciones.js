993c1ee7667fc96c2cf6e07c042ab491c4813e1a
Erick <vasquez.castro.andres@gmail.com>
2025-01-01 16:37:13 -0400

    POO--mas funciones

--- javascript/objetos/04-funciones.js
+++ javascript/objetos/04-funciones.js
@@ -17,3 +17,42 @@ function of(Fn , arg){
 }
 let user1 = of(Usuario, 'Chanchito');
 console.log(user1);
+
+
+function returned(){
+    return function(){
+        console.log('Hola Mundo');
+    }
+}
+let saludo = returned();
+saludo();
+
+
+
+
+function Punto(x,y){
+    this.x = x;
+    this.y = y;
+    this.dibujar = function() {console.log ('Dibujandoo...');}
+}
+
+let punto = {z:7};
+Punto.call(punto,1,2);
+
+console.log(punto);
+
+
+//Punto.apply(punto,[1,2]);
+
+
+
+
+/*
+const Point = new Function('x','y',`this.x = x;
+    this.y = y;
+    this.dibujar = function() {console.log ('Dibujandoo...');}`
+);
+
+const p = new Point(1,2);
+console.log(p);
+*/
