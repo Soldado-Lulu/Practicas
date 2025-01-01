ae598e9e23c6e2227fedb8ff303e304be67b4eb1
Erick <vasquez.castro.andres@gmail.com>
2025-01-01 16:23:56 -0400

    POO

--- javascript/objetos/01-usuarios.js
+++ javascript/objetos/01-usuarios.js
@@ -10,3 +10,7 @@ function crearUsuario(name, email){
     };
 }
 
+
+let user1 = crearUsuario('erick', 'vasquez@gmail.com');
+let user2 = crearUsuario('america', 'rojas@gmail.com');
+console.log(user1,user2 );
---
+++ javascript/objetos/03-atajos.js
@@ -0,0 +1,17 @@
+//let obj = {};
+
+//let obj = new Object();
+
+/*
+new Array(); []
+new String(); " "  ''  
+new Number(); 12
+new Boolean(); true false
+*/
+
+
+function Usuario(){
+    this.name ="chanchito feliz";
+}
+let user =new Usuario();
+console.log(user.constructor) 
